46880d1ca516b53555d179577c30ee7539233360
Paul Beynon <paul.beynon@gmail.com>
2019-12-08 00:35:20 +1100

    init

---
+++ .babelrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      corejs: 3,
+      targets: {
+        node: '10.15.1',
+        browsers: [
+          'chrome >= 52',
+          'fireFox >= 44',
+          'safari >= 7',
+          'ie >= 11',
+          'edge >= 12',
+        ],
+      },
+      useBuiltIns: 'usage',
+    }],
+  ],
+  plugins: [
+    ['module-resolver', {
+      root: ['./src'],
+      alias: {
+        '@': './src',
+        '#': './tests',
+      },
+    }],
+  ],
+};

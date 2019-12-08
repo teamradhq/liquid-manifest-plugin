0792383b79718694b53fe098d7eb93afe0bf41ba
Paul Beynon <paul.beynon@gmail.com>
2019-12-08 12:09:07 +1100

    ESLint script

--- tests/lib/liquid/index.test.js
+++ tests/lib/liquid/index.test.js
@@ -3,7 +3,7 @@ import liquid from '@/lib/liquid';
 describe('lib/liquid', () => {
   describe.each(Object.keys(liquid))('%s', (test) => {
     it('should always return a string', () => {
-      expect(typeof liquid[test]('anything')).toBe('string')
+      expect(typeof liquid[test]('anything')).toBe('string');
     });
-  })
+  });
 });
